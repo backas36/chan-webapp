724e16854ee5af64484b6e0ab4fcce358d5f09d3
backas36 <backas36@gmail.com>
2022-12-21 17:01:56 +0800

    add page help info in ingredient page

--- src/layout/PageWrapper.js
+++ src/layout/PageWrapper.js
@@ -1,7 +1,7 @@
 import { Box, Container } from "@mui/system"
 import PageTItle from "../components/title/PageTItle"
 
-const PageWrapper = ({ title, children }) => {
+const PageWrapper = ({ title, children, extraComp }) => {
   return (
     <Container maxWidth="lg" sx={{ width: "100%" }}>
       <Box
@@ -10,10 +10,11 @@ const PageWrapper = ({ title, children }) => {
           flexDirection: "row",
           display: "flex",
           alignItems: "center",
-          justifyContent: "space-between",
+          justifyContent: "flex-start",
         }}
       >
         <PageTItle title={title} />
+        {extraComp && extraComp}
       </Box>
       {children}
     </Container>
