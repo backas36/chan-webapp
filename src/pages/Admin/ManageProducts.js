4e47c337a0932655ec9cb4fafe0ce0a910e8bfd7
backas36 <backas36@gmail.com>
2022-12-23 23:23:06 +0800

    add help infomation

--- src/pages/Admin/ManageProducts.js
+++ src/pages/Admin/ManageProducts.js
@@ -1,15 +1,45 @@
+import { Help } from "@mui/icons-material"
+import { Alert, AlertTitle, IconButton } from "@mui/material"
 import { useTranslation } from "react-i18next"
+import LinkBtn from "../../components/buttons/LinkBtn"
+import MDialog from "../../components/dialog/MDialog"
 import ManagePoTable from "../../features/products/components/ManagePoTable"
 import useTitle from "../../hooks/useTitle"
+import useToggle from "../../hooks/useToggle"
 import MainWrapper from "../../layout/MainWrapper"
 import PageWrapper from "../../layout/PageWrapper"
 
 const ManageProducts = () => {
   const { t } = useTranslation()
+  const { visible, setToggleStatus } = useToggle(false)
+
   useTitle(t("ManageProducts"))
   return (
     <MainWrapper>
-      <PageWrapper title={t("ManageProducts")}>
+      <PageWrapper
+        title={t("ManageProducts")}
+        extraComp={
+          <MDialog
+            open={visible}
+            handleClose={() => setToggleStatus(false)}
+            title={t("help")}
+            dialogBtn={
+              <IconButton onClick={() => setToggleStatus(true)} color="success">
+                <Help />
+              </IconButton>
+            }
+          >
+            <Alert severity="info">
+              <AlertTitle>{t("info")}</AlertTitle>
+              {t("categoryNameHelpDesc")}
+              <LinkBtn
+                linkName="Product Categories"
+                linkPath="/admin/product-categories"
+              />
+            </Alert>
+          </MDialog>
+        }
+      >
         <ManagePoTable />
       </PageWrapper>
     </MainWrapper>
--- src/pages/Admin/Purchases.js
+++ src/pages/Admin/Purchases.js
@@ -1,15 +1,44 @@
+import { Help } from "@mui/icons-material"
+import { Alert, AlertTitle, IconButton } from "@mui/material"
 import { useTranslation } from "react-i18next"
+import LinkBtn from "../../components/buttons/LinkBtn"
+import MDialog from "../../components/dialog/MDialog"
 import PurchasesTable from "../../features/purchases/components/PurchasesTable"
 import useTitle from "../../hooks/useTitle"
+import useToggle from "../../hooks/useToggle"
 import MainWrapper from "../../layout/MainWrapper"
 import PageWrapper from "../../layout/PageWrapper"
 
 const Purchases = () => {
   const { t } = useTranslation()
+  const { visible, setToggleStatus } = useToggle(false)
+
   useTitle(t("Purchases"))
   return (
     <MainWrapper>
-      <PageWrapper title={t("managePurchases")}>
+      <PageWrapper
+        title={t("managePurchases")}
+        extraComp={
+          <MDialog
+            open={visible}
+            handleClose={() => setToggleStatus(false)}
+            title={t("help")}
+            dialogBtn={
+              <IconButton onClick={() => setToggleStatus(true)} color="success">
+                <Help />
+              </IconButton>
+            }
+          >
+            <Alert severity="info">
+              <AlertTitle>{t("info")}</AlertTitle>
+              {t("ingredientNameHelpDesc")}
+              <br />
+              <LinkBtn linkName="Suppliers" linkPath="/admin/suppliers" />
+              <LinkBtn linkName="Ingredients" linkPath="/admin/ingredients" />
+            </Alert>
+          </MDialog>
+        }
+      >
         <PurchasesTable />
       </PageWrapper>
     </MainWrapper>
